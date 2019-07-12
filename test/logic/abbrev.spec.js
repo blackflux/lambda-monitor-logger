bbd0af47062e6f7e35c9ee75e0027f57832455ed
Kodi Ackerman <kodiackerman@msn.com>
2019-07-12 16:07:39 -0700

    amend: updated failing tests

--- test/logic/abbrev.spec.js
+++ test/logic/abbrev.spec.js
@@ -38,9 +38,10 @@ describe('Testing abbrev', () => {
   it('Testing truncating string array', () => {
     expect(abbrev({
       one: createAscArray(1000, true)
-    })).to.equal(
+    })).to.be.oneOf([
+      "{ one: [ '0','1','2','3','4','5','6','7','8','9','10','11','12','13',... 986 more items ] }",
       "{ one: [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', ... 986 more items ] }"
-    );
+    ]);
   });
 
   it('Testing truncating array of string array', () => {
@@ -60,10 +61,12 @@ describe('Testing abbrev', () => {
         }
         return e;
       })
-    })).to.equal(
+    })).to.be.oneOf([
       "{ one: [ '0',[ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', ... 986 more items ]"
+      + ",'2','3','4','5','6','7','8','9','10','11','12','13',... 986 more items ] }",
+      "{ one: [ '0',[ '0','1','2','3','4','5','6','7','8','9','10','11','12','13',... 986 more items ]"
       + ",'2','3','4','5','6','7','8','9','10','11','12','13',... 986 more items ] }"
-    );
+    ]);
   });
 
   it('Testing truncating long string', () => {
