8bee6d63c1ce7ad45779e829a951688340c0cb19
Lukas Siemon <github@blackflux.com>
2021-09-29 08:37:57 -0700

    feat: added json logging

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const logger = require('./logic/logger');
 const abbrev = require('./logic/abbrev');
+const json = require('./logic/json');
 const datadog = require('./logic/datadog');
 const s3 = require('./logic/s3');
 
 module.exports.logger = logger;
 module.exports.abbrev = abbrev;
+module.exports.json = json;
 module.exports.datadog = datadog;
 module.exports.s3 = s3;
